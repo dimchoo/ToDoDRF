d5fa0637cabb765478517af1ddf7ae6fd5b42c22
dimchoo <imdimchoo@gmail.com>
2022-04-02 15:57:24 +0300

    Initial Commit

--- backend/users/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from users.models import UserDRF
-
-
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = UserDRF
-        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'phone_number', 'photo']
--- backend/users/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from users.models import UserDRF
-from users.serializers import UserSerializer
-
-
-class UserViewSet(ModelViewSet):
-    serializer_class = UserSerializer
-    queryset = UserDRF.objects.all()
