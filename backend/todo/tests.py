a79b9f5f8c7ec0147cc3a1082ed148a3350f2ea2
dimchoo <imdimchoo@gmail.com>
2022-04-28 14:22:49 +0300

    Lesson 8

--- backend/todo/tests.py
+++ backend/todo/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
+from users.models import UserDRF
+from mixer.backend.django import mixer
+from todo.models import Project
 
-# Create your tests here.
+
+class TestProjectClientAPI(TestCase):
+    def setUp(self) -> None:
+        self.user = UserDRF.objects.create_superuser(username='admin', email='a@dmin.com', password='qwerty')
+        self.projects = mixer.cycle(5).blend(Project, created_by=self.user)
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    # Написать минимум один тест для API, используя APIClient.
+    def test_get_list_authorized(self):
+        response = self.client.get('/api/projects/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # Данные для тестов удобно создавать, используя mixer.
+    def test_get_len_of_projects(self):
+        response = self.client.get('/api/projects/')
+        print(response.data)
+        self.assertEqual(len(response.data), 5)
+
+
+# Написать минимум один тест для API, используя APITestCase.
+class TestTaskApi(APITestCase):
+    def setUp(self) -> None:
+        self.user = UserDRF.objects.create_superuser(username='admin', email='a@dmin.com', password='qwerty')
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_list_authorized(self):
+        response = self.client.get('/api/tasks/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory
+from users.views import UserViewSet
 
-# Create your tests here.
+
+class TestUserAPI(TestCase):
+    # Написать минимум один тест для API, используя APIRequestFactory.
+    def test_get_list_unauthorized(self):
+        factory = APIRequestFactory()
+        request = factory.get('api/users/')
+        view = UserViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
